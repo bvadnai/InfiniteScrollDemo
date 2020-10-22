e25dfa91d4dc888bc7c0bf68569ea0629696c62c
Balazs Vadnai <11341356+bvadnai@users.noreply.github.com>
2020-10-22 23:21:26 +0200

    Add UIColor extension with color palette

---
+++ InfiniteScrollDemo/Extensions/UIColor.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor.swift
+//  InfiniteScrollDemo
+//
+//  Created by Balazs Vadnai on 2020. 10. 22..
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var black: UIColor {
+        return UIColor(white: 50.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var warmGrey: UIColor {
+        return UIColor(white: 155.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var blackTwo: UIColor {
+        return UIColor(white: 33.0 / 255.0, alpha: 0.72)
+    }
+
+    @nonobjc class var blackThree: UIColor {
+        return UIColor(white: 23.0 / 255.0, alpha: 1.0)
+    }
+}
