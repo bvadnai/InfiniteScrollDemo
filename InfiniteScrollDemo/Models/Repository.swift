f99d4c40d0bcfbe62e88e8a8ab6600e17c59ecf4
Balazs Vadnai <11341356+bvadnai@users.noreply.github.com>
2020-10-22 23:30:52 +0200

    Add Repository model

---
+++ InfiniteScrollDemo/Models/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  Repository.swift
+//  InfiniteScrollDemo
+//
+//  Created by Balazs Vadnai on 2020. 10. 22..
+//
+
+import Foundation
+
+struct Repository {
+    let id: Int
+    let name: String
+    let fullName: String
+    let url: URL
+    let avatarURL: URL
+    let score: Int
+    let description: String
+    let forksCount: Int
+    let openIssuesCount: Int
+    let watchersCount: Int
+}
