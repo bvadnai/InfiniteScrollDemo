7c68c2ce05f57e04e352882afd7869c80b9d12f5
Balazs Vadnai <11341356+bvadnai@users.noreply.github.com>
2020-10-23 03:01:39 +0200

    Add layout constants

--- InfiniteScrollDemo/Helpers/Constants.swift
+++ InfiniteScrollDemo/Helpers/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreGraphics
 
 enum ServerKey {
     static let message = "message"
@@ -26,4 +27,30 @@ enum ServerKey {
     static let forksCount = "forks_count"
     static let openIssuesCount = "open_issues_count"
     static let watchers = "watchers"
+}
+
+enum LayoutSize {
+    static let pt1: CGFloat = 1
+    static let pt4: CGFloat = 4
+    static let pt8: CGFloat = 8
+    static let pt11: CGFloat = 11
+    static let pt13: CGFloat = 13
+    static let pt15: CGFloat = 15
+    static let pt16: CGFloat = 16
+    static let pt20: CGFloat = 20
+    static let pt22: CGFloat = 22
+    static let pt23: CGFloat = 23
+    static let pt24: CGFloat = 24
+    static let pt26: CGFloat = 26
+    static let pt30: CGFloat = 30
+    static let pt32: CGFloat = 32
+    static let pt34: CGFloat = 34
+    static let pt40: CGFloat = 40
+    static let pt48: CGFloat = 48
+    static let pt58: CGFloat = 58
+    static let pt70: CGFloat = 70
+    static let pt78: CGFloat = 78
+    static let pt108: CGFloat = 108
+    static let pt141: CGFloat = 141
+    static let pt176: CGFloat = 176
 }
