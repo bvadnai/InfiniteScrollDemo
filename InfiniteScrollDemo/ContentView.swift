a8b71c1555638bb7dd63af8b14a1022f9c82781b
Balazs Vadnai <11341356+bvadnai@users.noreply.github.com>
2020-11-16 03:39:42 +0100

    Add project skeleton

---
+++ InfiniteScrollDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InfiniteScrollDemo
+//
+//  Created by Balazs Vadnai on 2020. 11. 16..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InfiniteScrollDemo/InfiniteScrollDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollDemoApp.swift
+//  InfiniteScrollDemo
+//
+//  Created by Balazs Vadnai on 2020. 11. 16..
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
